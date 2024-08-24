f1868258d409b84634f2c50bff4a292217f5f0ff
bluegown <alan0308@naver.com>
2024-08-24 23:39:50 +0900

    :bug: Fix: RedisConfig 수정

--- src/main/java/com/heartfoilo/demo/config/RedisConfig.java
+++ src/main/java/com/heartfoilo/demo/config/RedisConfig.java
@@ -14,13 +14,13 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @EnableRedisRepositories
 public class RedisConfig {
 
-    @Value("${spring.data.redis.host}")
+    @Value("${spring.redis.host}")
     private String host;
 
-    @Value("${spring.data.redis.port}")
+    @Value("${spring.redis.port}")
     private int port;
 
-    @Value("${spring.data.redis.password}")
+    @Value("${spring.redis.password}")
     private String password;
 
 
