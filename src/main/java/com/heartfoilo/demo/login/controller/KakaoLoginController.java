985ea81c0e2b178d6bfa1e2e2a07e97070e03e27
bluegown <alan0308@naver.com>
2024-08-24 23:53:10 +0900

    :hammer: Refactor: KakaoLoginController RequestMapping 추가

--- src/main/java/com/heartfoilo/demo/login/controller/KakaoLoginController.java
+++ src/main/java/com/heartfoilo/demo/login/controller/KakaoLoginController.java
@@ -19,7 +19,7 @@ import java.util.NoSuchElementException;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-
+@RequestMapping("")
 public class KakaoLoginController {
 
     private final KakaoService kakaoService;
