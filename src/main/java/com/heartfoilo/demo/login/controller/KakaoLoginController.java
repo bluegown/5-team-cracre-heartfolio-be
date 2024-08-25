a06c29cea5cae38b18c7440c8220621fe7e712a3
bluegown <alan0308@naver.com>
2024-08-25 21:33:00 +0900

    :memo: Refactor: KakaoLoginController ResponseEntity 반환형 변경

--- src/main/java/com/heartfoilo/demo/login/controller/KakaoLoginController.java
+++ src/main/java/com/heartfoilo/demo/login/controller/KakaoLoginController.java
@@ -25,9 +25,8 @@ import java.util.NoSuchElementException;
 public class KakaoLoginController {
 
     private final KakaoService kakaoService;
-
     @GetMapping("/oauth")
-    public ResponseEntity<?> callback(@RequestParam("code") String code, HttpServletResponse response) throws IOException {
+    public ResponseEntity<LoginResponse> callback(@RequestParam("code") String code, HttpServletResponse response) throws IOException {
         try {
             // 인가 코드를 사용하여 액세스 토큰을 가져옴
             String accessToken = kakaoService.getAccessTokenFromKakao(code);
@@ -35,15 +34,11 @@ public class KakaoLoginController {
             // 액세스 토큰으로 사용자 정보 가져오기
             KakaoUserInfoResponseDto userInfo = kakaoService.getUserInfo(accessToken);
 
-            // 사용자 정보로 로그인 처리 및 JWT 토큰 생성
+            // 사용자 정보로 로그인 처리 및 LoginResponse 객체 생성
             LoginResponse kakaoUserResponse = kakaoService.kakaoUserLogin(userInfo);
 
-            // JWT 토큰을 Authorization 헤더에 추가
-            HttpHeaders headers = new HttpHeaders();
-            headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + kakaoUserResponse.getToken().getAccessToken());
-
-            // JWT 토큰을 포함한 헤더와 함께 빈 본문으로 응답 반환
-            return new ResponseEntity<>(null, headers, HttpStatus.OK);
+            // JSON으로 직렬화되어 반환됨
+            return ResponseEntity.ok(kakaoUserResponse);
         } catch (NoSuchElementException e) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Item Not Found");
         }
