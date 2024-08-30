35bf4f091a24a087652b36d51a552bc8c22f42fe
limzzum <chfhddl1234@naver.com>
2024-08-31 00:40:47 +0900

    :sparkles: Feat : 주식정보 polling 스케줄러 추가

---
+++ src/main/java/com/heartfoilo/demo/util/StockInfoPollingScheduler.java
@@ -0,0 +1,123 @@
+package com.heartfoilo.demo.util;
+
+import com.heartfoilo.demo.domain.stock.entity.Stock;
+import com.heartfoilo.demo.domain.stock.repository.StockRepository;
+import com.heartfoilo.demo.domain.webSocket.dto.StockSocketInfoDto;
+import com.heartfoilo.demo.dto.RequestOauthDto;
+import jakarta.annotation.PostConstruct;
+import java.util.List;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class StockInfoPollingScheduler {
+
+    private final StockRepository stockRepository;
+    private final RedisUtil redisUtil;
+    private List<Stock> stocks;
+    private WebClient webClient = WebClient.builder().build();;
+
+    @Value("${korea-invest.host}")
+    private String host;
+
+    @Value("${korea-invest.port}")
+    private String port;
+
+    @Value("${korea-invest.url}")
+    private String url;
+
+    @Value("${korea-invest.oauth-url}")
+    private String oauthUrl;
+
+    @Value("${korea-invest.app-key}")
+    private String appKey;
+
+    @Value("${korea-invest.app-secret}")
+    private String appSecret;
+
+    @Value("${korea-invest.tr-id}")
+    private String trId;
+
+    private String token;
+
+    @PostConstruct
+    public void getStocks(){
+        stocks = stockRepository.findAll();
+        token = getOauthToken();
+    }
+
+    @Scheduled(fixedRate = 5000)
+    public void pollingStockInfo(){
+        String type = "NYS";
+        for (Stock stock : stocks) {
+            if(stock.getType().equals("NASDAQ")){
+                type = "NAS";
+            }
+            updateStockInfo(type, stock.getSymbol());
+        }
+
+    }
+
+    @Scheduled(cron = "0 0 23 * * ?")
+    public void updateToken(){
+        token = getOauthToken();
+    }
+
+    private void updateStockInfo(String type, String symbol){
+        try {
+            Map result = webClient
+                .get()
+                .uri(uriBuilder ->
+                    uriBuilder
+                        .scheme("https")
+                        .host(host)
+                        .port(port)
+                        .path(url)
+                        .queryParam("AUTH", "")
+                        .queryParam("EXCD", type)
+                        .queryParam("SYMB", symbol)
+                        .build())
+                .header(HttpHeaders.AUTHORIZATION,
+                    "Bearer "+token)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .header("appkey", appKey)
+                .header("appsecret", appSecret)
+                .header("tr_id", trId)
+                .retrieve()
+                .bodyToMono(Map.class)
+                .block();
+            redisUtil.setStockInfoTemplate(symbol, new StockSocketInfoDto(
+                (Map<String, String>) result.get("output")));
+
+        }catch (Exception e){
+            log.error(e.getMessage());
+        }
+
+    }
+
+    private String getOauthToken(){
+        Map result = webClient
+            .post()
+            .uri(uriBuilder ->
+                uriBuilder
+                    .scheme("https")
+                    .host(host)
+                    .port(port)
+                    .path(oauthUrl)
+                    .build())
+            .bodyValue(RequestOauthDto.builder().grant_type("client_credentials").appkey(appKey).appsecret(appSecret).build())
+            .retrieve()
+            .bodyToMono(Map.class)
+            .block();
+        return String.valueOf(result.get("access_token"));
+    }
+}
